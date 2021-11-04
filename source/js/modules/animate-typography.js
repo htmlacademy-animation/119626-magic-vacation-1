1406c56b08e56f00581aeceed751d64d3ad1f4cc
bolshu <bolshu@MacBook-Pro-bolshu.local>
2021-11-05 00:52:12 +0300

    animate title

--- source/js/modules/animate-typography.js
+++ source/js/modules/animate-typography.js
@@ -1,44 +1,48 @@
 const CLASS_NAME = `animated-typography`;
 
 export default class AnimateTypography {
-  constructor(node, animation) {
-    this.node = node;
-    this.animation = animation;
+  constructor({node, duration, delay}) {
+    this._node = node;
+    this._duration = duration;
+    this._delay = delay;
   }
 
-  addAnimationClass() {
-    this.node.classList.add(CLASS_NAME);
+  _animate() {
+    setTimeout(() => {
+      this._node.classList.add(`${CLASS_NAME}--active`);
+    }, this._delay);
   }
 
-  wrapInSpan() {
+  _replaceNode() {
+    this._node.classList.add(CLASS_NAME);
     const fragment = document.createDocumentFragment();
-    const words = this.node.textContent.split(` `).map((word) => word.split(``));
+    const words = this._node.textContent.split(` `).map((word) => word.split(``));
 
-    words.forEach((word, i) => {
-      const wordFragment = document.createElement(`span`);
-      wordFragment.classList.add(`${CLASS_NAME}__word`);
+    words.forEach((word) => {
+      const spanWord = document.createElement(`span`);
+      spanWord.classList.add(`${CLASS_NAME}__word`);
 
       word.forEach((char) => {
-        const charInSpan = document.createElement(`span`);
-        charInSpan.classList.add(`${CLASS_NAME}__char`);
-        charInSpan.textContent = char;
-        wordFragment.appendChild(charInSpan);
+        const spanChar = document.createElement(`span`);
+        spanChar.classList.add(`${CLASS_NAME}__char`);
+
+        spanChar.textContent = char;
+        spanChar.style.animationDuration = `${this._duration}ms`;
+        spanChar.style.animationTimingFunction = `ease-in`;
+        spanChar.style.animationDelay = `${this._delay * Math.random()}ms`;
+
+        spanWord.appendChild(spanChar);
       });
 
-      fragment.appendChild(wordFragment);
-
-      if (i !== words.length - 1) {
-        const br = document.createElement(`br`);
-        fragment.appendChild(br);
-      }
+      fragment.appendChild(spanWord);
     });
 
-    this.node.innerHTML = null;
-    this.node.appendChild(fragment);
+    this._node.innerHTML = null;
+    this._node.appendChild(fragment);
   }
 
   init() {
-    this.addAnimationClass();
-    this.wrapInSpan();
+    this._replaceNode();
+    this._animate();
   }
 }
--- source/js/script.js
+++ source/js/script.js
@@ -26,13 +26,21 @@ const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
 
 const mainTitle = document.querySelector(`.intro__title`);
-const animateMainTitle = new AnimateTypography(mainTitle);
+const animateMainTitle = new AnimateTypography({node: mainTitle, duration: 300, delay: 200});
 animateMainTitle.init();
 
 const historyTitle = document.querySelector(`.slider__item-title`);
-const animateHistoryTitle = new AnimateTypography(historyTitle);
+const animateHistoryTitle = new AnimateTypography({node: historyTitle, duration: 300, delay: 200});
 animateHistoryTitle.init();
 
 const prizesTitle = document.querySelector(`.prizes__title`);
-const animatePrizesTitle = new AnimateTypography(prizesTitle);
+const animatePrizesTitle = new AnimateTypography({node: prizesTitle, duration: 300, delay: 200});
 animatePrizesTitle.init();
+
+const rulesTitle = document.querySelector(`.rules__title`);
+const animateRulesTitle = new AnimateTypography({node: rulesTitle, duration: 300, delay: 200});
+animateRulesTitle.init();
+
+const gameTitle = document.querySelector(`.game__title`);
+const animateGameTitle = new AnimateTypography({node: gameTitle, duration: 300, delay: 200});
+animateGameTitle.init();
