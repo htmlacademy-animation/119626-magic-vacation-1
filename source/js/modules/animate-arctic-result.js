35a5fff9da4c3b37d67b85910f4df21ba7520df5
bolshu <bolshu@MacBook-Pro-bolshu.local>
2022-01-10 22:56:24 +0300

    canvas imgs snowflakes

--- source/js/modules/animate-arctic-result.js
+++ source/js/modules/animate-arctic-result.js
@@ -27,7 +27,7 @@ export default () => {
   const snowflake2ImgDom = new Image();
 
   const drawSnowflake2 = () => {
-    ctx.drawImage(snowflake2ImgDom, 790, 380, 120, 120);
+    ctx.drawImage(snowflake2ImgDom, 760, 400, 130, 130);
   };
 
   snowflake2ImgDom.onload = () => {
@@ -35,4 +35,82 @@ export default () => {
   };
 
   snowflake2ImgDom.src = `/img/module-4/win-primary-images/snowflake-2.png`;
+
+  // airplane
+  const airplaneImgDom = new Image();
+
+  const drawAirplane = () => {
+    ctx.drawImage(airplaneImgDom, 950, 230, 100, 100);
+  };
+
+  airplaneImgDom.onload = () => {
+    drawAirplane();
+  };
+
+  airplaneImgDom.src = `/img/module-4/win-primary-images/airplane.png`;
+
+  // back
+  const backImgDom = new Image();
+
+  const drawBack = () => {
+    ctx.drawImage(backImgDom, 388, 204, 586, 324);
+  };
+
+  backImgDom.onload = () => {
+    drawBack();
+  };
+
+  backImgDom.src = `/img/module-4/win-primary-images/back.png`;
+
+  // tree 1
+  const tree1ImgDom = new Image();
+
+  const drawTree1 = () => {
+    ctx.drawImage(tree1ImgDom, 740, 370, 38, 111);
+  };
+
+  tree1ImgDom.onload = () => {
+    drawTree1();
+  };
+
+  tree1ImgDom.src = `/img/module-4/win-primary-images/tree-1.png`;
+
+  // tree 2
+  const tree2ImgDom = new Image();
+
+  const drawTree2 = () => {
+    ctx.drawImage(tree2ImgDom, 690, 300, 50, 159);
+  };
+
+  tree2ImgDom.onload = () => {
+    drawTree2();
+  };
+
+  tree2ImgDom.src = `/img/module-4/win-primary-images/tree-2.png`;
+
+  // ice
+  const iceImgDom = new Image();
+
+  const drawIce = () => {
+    ctx.drawImage(iceImgDom, 460, 430, 370, 150);
+  };
+
+  iceImgDom.onload = () => {
+    drawIce();
+  };
+
+  iceImgDom.src = `/img/module-4/win-primary-images/ice.png`;
+
+  // sea calf
+  const seaCalfImgDom = new Image();
+
+  const drawSeaCalf = () => {
+    ctx.drawImage(seaCalfImgDom, 390, 210, 450, 450);
+  };
+
+  seaCalfImgDom.onload = () => {
+    drawSeaCalf();
+  };
+
+  seaCalfImgDom.src = `/img/module-4/win-primary-images/sea-calf-2.png`;
 };
