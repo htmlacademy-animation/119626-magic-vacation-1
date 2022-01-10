1cc020af9124d58a58db8bd4537f697480e544f1
bolshu <bolshu@MacBook-Pro-bolshu.local>
2022-01-10 22:33:47 +0300

    canvas imgs snowflakes

--- source/js/modules/animate-arctic-result.js
+++ source/js/modules/animate-arctic-result.js
@@ -3,29 +3,36 @@ export default () => {
   const wh = window.innerHeight;
 
   const resultCanvasDom = document.getElementById(`result-1`);
+  resultCanvasDom.width = ww;
+  resultCanvasDom.height = wh;
+
   const ctx = resultCanvasDom.getContext(`2d`);
+  ctx.width = ww;
+  ctx.height = wh;
 
-  const snowflake1 = {
-    width: 200,
-    height: 200,
-    top: 310,
-    left: 340,
+  // snowflake
+  const snowflake1ImgDom = new Image();
+
+  const drawSnowflake1 = () => {
+    ctx.drawImage(snowflake1ImgDom, 340, 310, 200, 200);
   };
 
-  const snowflakeImgDom = new Image();
-
-  const draw = () => {
-    resultCanvasDom.width = ww;
-    resultCanvasDom.height = wh;
-    ctx.width = ww;
-    ctx.height = wh;
-
-    ctx.drawImage(snowflakeImgDom, snowflake1.left, snowflake1.top, snowflake1.width, snowflake1.height);
+  snowflake1ImgDom.onload = () => {
+    drawSnowflake1();
   };
 
-  snowflakeImgDom.onload = () => {
-    draw();
+  snowflake1ImgDom.src = `/img/module-4/win-primary-images/snowflake-1.png`;
+
+  // snowflake 2
+  const snowflake2ImgDom = new Image();
+
+  const drawSnowflake2 = () => {
+    ctx.drawImage(snowflake2ImgDom, 790, 380, 120, 120);
   };
 
-  snowflakeImgDom.src = `/img/module-4/win-primary-images/snowflake.png`;
+  snowflake2ImgDom.onload = () => {
+    drawSnowflake2();
+  };
+
+  snowflake2ImgDom.src = `/img/module-4/win-primary-images/snowflake-2.png`;
 };
