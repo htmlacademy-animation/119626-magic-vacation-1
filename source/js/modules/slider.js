19e6aeb094a19402e97d3fad215960efcca45239
bolshu <bolshu@MacBook-Pro-bolshu.local>
2022-01-30 00:29:30 +0300

    m5-t1 rm bgs

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,31 +1,9 @@
 import Swiper from "swiper";
 
-const CN_BLUE = `body-theme-blue`;
-const CN_LIGHT_BLUE = `body-theme-light-blue`;
-const CN_PURPLE = `body-theme-purple`;
-const CN_DARK = `body-theme-dark`;
-
-const bodyThemeClasses = [
-  CN_BLUE,
-  CN_LIGHT_BLUE,
-  CN_PURPLE,
-  CN_DARK,
-];
-
-const removeTheme = () => {
-  bodyThemeClasses.forEach((cn) => {
-    document.body.classList.remove(cn);
-  });
-};
-
 export default () => {
   let storySlider;
-  let sliderContainer = document.getElementById(`story`);
-  sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
 
   const setSlider = function () {
-    document.body.classList.add(CN_DARK);
-
     if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
       storySlider = new Swiper(`.js-slider`, {
         pagination: {
@@ -38,13 +16,13 @@ export default () => {
         on: {
           slideChange: () => {
             if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
-              sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+              // TODO: render bg
             } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
-              sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
+              // TODO: render bg
             } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
-              sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
+              // TODO: render bg
             } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
-              sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
+              // TODO: render bg
             }
           },
           resize: () => {
@@ -71,20 +49,14 @@ export default () => {
         },
         on: {
           slideChange: () => {
-            removeTheme();
-
             if (storySlider.activeIndex === 0) {
-              sliderContainer.style.backgroundImage = `url("img/slide1.jpg")`;
-              document.body.classList.add(CN_PURPLE);
+              // TODO: render bg
             } else if (storySlider.activeIndex === 2) {
-              sliderContainer.style.backgroundImage = `url("img/slide2.jpg")`;
-              document.body.classList.add(CN_BLUE);
+              // TODO: render bg
             } else if (storySlider.activeIndex === 4) {
-              sliderContainer.style.backgroundImage = `url("img/slide3.jpg")`;
-              document.body.classList.add(CN_LIGHT_BLUE);
+              // TODO: render bg
             } else if (storySlider.activeIndex === 6) {
-              sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
-              document.body.classList.add(CN_DARK);
+              // TODO: render bg
             }
           },
           resize: () => {
