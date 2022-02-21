52af7018c9228b8a0b762bab263dc92505120174
bolshu <bolshu@MacBook-Pro-bolshu.local>
2022-02-21 22:29:43 +0300

    module5-task4 bubble border

--- source/js/webGL/shaders/fragmentShader.glsl
+++ source/js/webGL/shaders/fragmentShader.glsl
@@ -5,19 +5,45 @@ uniform float uHue;
 uniform vec2 uCanvasSize;
 uniform bool uShouldRenderBubbles;
 
-const vec3 uBubble1 = vec3(0.75, 0.5, 0.3);
-const vec3 uBubble2 = vec3(1.0, 1.0, 0.25);
-const vec3 uBubble3 = vec3(1.1, 1.45, 0.2);
+const float bubbleBorderWidth = 0.01;
+const vec4 bubbleBorderColor = vec4(1.0, 1.0, 1.0, 0.15);
+
+const vec3 bubble1 = vec3(0.75, 0.25, 0.3);
+const vec3 bubble2 = vec3(1.0, 1.0, 0.25);
+const vec3 bubble3 = vec3(1.2, 1.5, 0.2);
 
 varying vec2 vUv;
 
+vec4 getTextureWithBubbleBorder(vec4 texel, vec4 color) {
+  return vec4(mix(texel.rgb, color.rgb, color.a), texel.a);
+}
+
 vec4 getTextureWithBubble(vec3 bubble, vec4 texel) {
+  float radius = bubble.z;
+
   vec2 bubbleCoords = vec2(uCanvasSize.x * bubble.x, uCanvasSize.y * bubble.y);
 
   float dist = distance(gl_FragCoord.xy, bubbleCoords) / uCanvasSize.y;
 
-  if (dist < bubble.z) {
+  // distortion
+  if (dist < radius) {
     texel = texture2D(uMap, vUv + (dist * dist * dist));
+  }
+
+  // border
+  if (dist > radius && dist <= radius + bubbleBorderWidth) {
+    texel = getTextureWithBubbleBorder(texture2D(uMap, vUv), bubbleBorderColor);
+  }
+
+  // highlight
+  float highlightRadius = radius * 0.75;
+  vec2 highlightCoords = gl_FragCoord.xy - bubbleCoords;
+  float highlightAngle = atan(highlightCoords.y, highlightCoords.x);
+
+  bool shouldRenderHighlight = highlightAngle >= 2.0 && highlightAngle <= 3.0;
+
+  if (dist > highlightRadius && dist < highlightRadius + bubbleBorderWidth && shouldRenderHighlight) {
+    texel = getTextureWithBubbleBorder(texture2D(uMap, vUv), bubbleBorderColor);
   }
 
   return texel;
@@ -35,9 +61,9 @@ void main() {
   vec4 texel = texture2D(uMap, vUv);
 
   if (uShouldRenderBubbles) {
-    texel = getTextureWithBubble(uBubble1, texel);
-    texel = getTextureWithBubble(uBubble2, texel);
-    texel = getTextureWithBubble(uBubble3, texel);
+    texel = getTextureWithBubble(bubble1, texel);
+    texel = getTextureWithBubble(bubble2, texel);
+    texel = getTextureWithBubble(bubble3, texel);
   }
 
 	if (uHue == 0.0) {
