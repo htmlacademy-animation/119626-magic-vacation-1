efc171b11d73c1e50fd793c4415bb1c5bb40e8c3
Keks <keks@htmlacademy.ru>
2022-03-27 09:23:26 +0300

    :heavy_check_mark: Сборка #22

--- 22/js/script.js
+++ 22/js/script.js
@@ -62866,14 +62866,12 @@ class Scene3D {
     this.zCoordinateMin = 0.1;
     this.zCoordinateMax = 1000;
 
-    this.animationId = null;
-
     this.init();
   }
 
   getRandomHue() {
     const HUE_MIN = 0.2;
-    const HUE_MAX = 0.7;
+    const HUE_MAX = 0.5;
 
     const randomHue = Math.random();
 
@@ -62888,15 +62886,13 @@ class Scene3D {
   }
 
   setMaterial(scene) {
-    const hue = scene.shouldRenderBubbles ? this.getRandomHue() : 0;
-
     this.material = new three__WEBPACK_IMPORTED_MODULE_0__["RawShaderMaterial"]({
       uniforms: {
         uMap: {
           value: scene.texture,
         },
         uHue: {
-          value: hue,
+          value: this.getRandomHue(),
         },
         uCanvasSize: {
           value: [this.width, this.height],
@@ -62952,8 +62948,9 @@ class Scene3D {
   }
 
   stop() {
-    cancelAnimationFrame(this.animationId);
-    this.animationId = null;
+    if (this.animation) {
+      this.animation.stop();
+    }
   }
 
   start() {
@@ -63276,7 +63273,7 @@ animationLauncher.init();
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = {sourceCode:"precision mediump float;uniform sampler2D uMap;uniform float uHue;uniform float uProgress;uniform vec2 uCanvasSize;uniform bool uShouldRenderBubbles;const float bubbleBorderWidth=0.01;const vec4 bubbleBorderColor=vec4(1.,1.,1.,0.15);const vec3 bubble1=vec3(0.75,0.25,0.3);const vec3 bubble2=vec3(1.,1.,0.25);const vec3 bubble3=vec3(1.2,1.5,0.2);varying vec2 vUv;vec4 getTextureWithBubbleBorder(vec4 texel,vec4 color){return vec4(mix(texel.rgb,color.rgb,color.a),texel.a);}vec4 getTextureWithBubble(vec3 bubble,vec4 texel){float radius=bubble.z;vec2 bubbleCoords=vec2(uCanvasSize.x*bubble.x,uCanvasSize.y*bubble.y);float dist=distance(gl_FragCoord.xy,bubbleCoords)/uCanvasSize.y;if(dist<radius){texel=texture2D(uMap,vUv+dist*dist*dist);}if(dist>radius&&dist<=radius+bubbleBorderWidth){texel=getTextureWithBubbleBorder(texture2D(uMap,vUv),bubbleBorderColor);}float highlightRadius=radius*0.75;vec2 highlightCoords=gl_FragCoord.xy-bubbleCoords;float highlightAngleStart=2.;float highlightAngleEnd=2.5;float highlightAngle=atan(highlightCoords.y,highlightCoords.x);bool shouldRenderHighlight=highlightAngle>=highlightAngleStart&&highlightAngle<=highlightAngleEnd;if(shouldRenderHighlight&&dist>highlightRadius&&dist<highlightRadius+bubbleBorderWidth){texel=getTextureWithBubbleBorder(texture2D(uMap,vUv),bubbleBorderColor);}return texel;}vec3 hueShift(vec3 color,float hue){const vec3 k=vec3(0.57735,0.57735,0.57735);float cosAngle=cos(hue);return vec3(color*cosAngle+cross(k,color)*sin(hue)+k*dot(k,color)*(1.-cosAngle));}void main(){vec4 texel=texture2D(uMap,vUv);if(uShouldRenderBubbles){texel=getTextureWithBubble(bubble1,texel);texel=getTextureWithBubble(bubble2,texel);texel=getTextureWithBubble(bubble3,texel);vec3 hueShifted=hueShift(texel.rgb,uHue*uProgress);gl_FragColor=vec4(hueShifted.rgb,1);}else{gl_FragColor=texel;}}",uniforms:{uMap:{variableType:"sampler2D",variableName:"uMap"},uHue:{variableType:"float",variableName:"uHue"},uProgress:{variableType:"float",variableName:"uProgress"},uCanvasSize:{variableType:"vec2",variableName:"uCanvasSize"},uShouldRenderBubbles:{variableType:"bool",variableName:"uShouldRenderBubbles"}},consts:{}}
+module.exports = {sourceCode:"precision mediump float;uniform sampler2D uMap;uniform float uHue;uniform float uProgress;uniform vec2 uCanvasSize;uniform bool uShouldRenderBubbles;const float bubbleBorderWidth=0.01;const vec4 bubbleBorderColor=vec4(1.,1.,1.,0.15);const vec3 bubble1=vec3(0.75,0.25,0.3);const vec3 bubble2=vec3(1.,1.,0.25);const vec3 bubble3=vec3(1.2,1.5,0.2);varying vec2 vUv;vec4 getTextureWithBubbleBorder(vec4 texel,vec4 color){return vec4(mix(texel.rgb,color.rgb,color.a),texel.a);}vec4 getTextureWithBubble(vec3 bubble,vec4 texel){float radius=bubble.z;vec2 bubbleCoords=vec2(uCanvasSize.x*bubble.x,uCanvasSize.y*bubble.y);float dist=distance(gl_FragCoord.xy,bubbleCoords)/uCanvasSize.y;if(dist<radius){texel=texture2D(uMap,vUv+dist*dist*dist);}if(dist>radius&&dist<=radius+bubbleBorderWidth){texel=getTextureWithBubbleBorder(texture2D(uMap,vUv),bubbleBorderColor);}float highlightRadius=radius*0.75;vec2 highlightCoords=gl_FragCoord.xy-bubbleCoords;float highlightAngleStart=2.;float highlightAngleEnd=2.5;float highlightAngle=atan(highlightCoords.y,highlightCoords.x);bool shouldRenderHighlight=highlightAngle>=highlightAngleStart&&highlightAngle<=highlightAngleEnd;if(shouldRenderHighlight&&dist>highlightRadius&&dist<highlightRadius+bubbleBorderWidth){texel=getTextureWithBubbleBorder(texture2D(uMap,vUv),bubbleBorderColor);}return texel;}vec3 hueShift(vec3 color,float hue){const vec3 k=vec3(0.57735,0.57735,0.57735);float cosAngle=cos(hue);return vec3(color*cosAngle+cross(k,color)*sin(hue)+k*dot(k,color)*(1.-cosAngle));}void main(){vec4 texel=texture2D(uMap,vUv);if(uShouldRenderBubbles){texel=getTextureWithBubble(bubble1,texel);texel=getTextureWithBubble(bubble2,texel);texel=getTextureWithBubble(bubble3,texel);float hue=uHue*uProgress*-1.;vec3 hueShifted=hueShift(texel.rgb,hue);gl_FragColor=vec4(hueShifted.rgb,1);}else{gl_FragColor=texel;}}",uniforms:{uMap:{variableType:"sampler2D",variableName:"uMap"},uHue:{variableType:"float",variableName:"uHue"},uProgress:{variableType:"float",variableName:"uProgress"},uCanvasSize:{variableType:"vec2",variableName:"uCanvasSize"},uShouldRenderBubbles:{variableType:"bool",variableName:"uShouldRenderBubbles"}},consts:{}}
 
 /***/ }),
 
