deae4918155ecbc98a2005eae2e2a65e69d3928d
bolshu <bolshu@MacBook-Pro-bolshu.local>
2022-04-18 08:45:14 +0300

    module6-task5

--- source/js/modules/scene-3d/scene-3d.js
+++ source/js/modules/scene-3d/scene-3d.js
@@ -11,7 +11,7 @@ export default class Scene3D {
     this.aspectRatio = this.width / this.height;
     this.perspectiveAngle = 35;
     this.zCoordinateMin = 0.1;
-    this.zCoordinateMax = 1000;
+    this.zCoordinateMax = 3000; // TODO: change to 1000
 
     this.animationId = null;
     this.material = null;
